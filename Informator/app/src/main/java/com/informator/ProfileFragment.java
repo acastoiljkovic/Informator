56a4e9b301d906b8c3b3dfe038fb76071cd069f3
djlaza007 <lazar.stoiljkovic@elfak.rs>
2020-05-01 14:19:05 +0200

    fragmenti za razlicite stranice

--- Informator/app/src/main/java/com/informator/ProfileFragment.java
+++ Informator/app/src/main/java/com/informator/ProfileFragment.java
@@ -2,6 +2,8 @@ package com.informator;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -11,9 +13,22 @@ import androidx.fragment.app.Fragment;
 
 public class ProfileFragment extends Fragment {
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_profile,container,false);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.fragment_profile_app_bar,menu);
+        super.onCreateOptionsMenu(menu,inflater);
+        return;
     }
 }
