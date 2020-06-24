5c7cd82c6332f146e4e700de5c3c408fee2ae0d9
Aca Stoiljkovic <aca.stoiljkovic@elfak.rs>
2020-06-24 16:41:42 +0200

    -user

---
+++ Informator/app/src/main/java/com/informator/services/LocationTracker.java
@@ -0,0 +1,30 @@
+package com.informator.services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+import com.google.firebase.database.FirebaseDatabase;
+import com.informator.data.StoredData;
+
+public class LocationTracker extends Service {
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Toast.makeText(this, "Service started by user.", Toast.LENGTH_LONG).show();
+
+        return START_STICKY;
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "Service destroyed by user.", Toast.LENGTH_LONG).show();
+    }
+}
